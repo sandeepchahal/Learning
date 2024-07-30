44fa67a9334011af673112579f8ae0ca6a888405
sandeepsingh <sandeepchahal433@gmail.com>
2024-07-30 11:40:55 +0530

    removed MS Demo

--- Microservice Demo/OrderDemo/OrderAPI/OrderAPI/Models/Order.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OrderAPI.Models;
-
-public class Order
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-}
