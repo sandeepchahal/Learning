cbe2ea93ea3bfc1d9afb0bf3a336ff2de2826540
sandeepsingh <sandeepchahal433@gmail.com>
2024-08-19 17:44:46 +0530

    minor fixes in compose file and cart service

--- Nagarro/Microservice Advance/CartService/ProductReservationWatchService.cs
+++ Nagarro/Microservice Advance/CartService/ProductReservationWatchService.cs
@@ -1,38 +1,51 @@
 using CartService.Implementation;
 namespace CartService;
 
-public class ProductReservationWatchService(IServiceScopeFactory serviceScopeFactory):BackgroundService
+public class ProductReservationWatchService(IServiceScopeFactory serviceScopeFactory, ILogger<ProductReservationWatchService> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var itemsToRemove = new List<(int userId, int productId, int productDetailId)>();
+
         while (!stoppingToken.IsCancellationRequested)
         {
             try
             {
                 using var scope = serviceScopeFactory.CreateScope();
                 var cartService = scope.ServiceProvider.GetRequiredService<ICartService>();
-
                 var result = cartService.GetAllItems();
-
+                logger.LogInformation($"Items count- {result.Count}");
+                itemsToRemove.Clear();
                 foreach (var dict in result)
                 {
                     foreach (var item in dict.Value)
                     {
-                        if (DateTime.Now <= item.ExpirationTime) continue;
-                        var (removed, message) =
-                            await cartService.RemoveItem(dict.Key, item.ProductId, item.ProductDetailId);
-                        Console.WriteLine(
-                            removed
-                                ? $"Removed expired item: UserId={dict.Key}, ProductId={item.ProductId}, ProductDetailId={item.ProductDetailId}"
-                                : $"Failed to remove expired item: UserId={dict.Key}, ProductId={item.ProductId}, ProductDetailId={item.ProductDetailId}. Reason: {message}");
+                        logger.LogInformation($"Current Time - {DateTime.Now}");
+                        logger.LogInformation($"{item.ExpirationTime}");
+
+                        if (DateTime.Now > item.ExpirationTime)
+                        {
+                            itemsToRemove.Add((dict.Key, item.ProductId, item.ProductDetailId));
+                        }
+
                     }
+                }
+                logger.LogInformation($"Items To Delete count- {itemsToRemove.Count}");
+                foreach (var (userId, productId, productDetailId) in itemsToRemove)
+                {
+                    var (removed, message) = await cartService.RemoveItem(userId, productId, productDetailId);
+
+                    logger.LogInformation(
+                        removed
+                            ? $"Removed expired item: UserId={userId}, ProductId={productId}, ProductDetailId={productDetailId}"
+                            : $"Failed to remove expired item: UserId={userId}, ProductId={productId}, ProductDetailId={productDetailId}. Reason: {message}");
                 }
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"An error occurred while cleaning up expired cart items: {ex.Message}");
+                logger.LogError($"An error occurred while cleaning up expired cart items: {ex.Message}");
             }
-            await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken); // Run every 5 minutes
+            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken); // Run every 1 minute
         }
     }
 }
